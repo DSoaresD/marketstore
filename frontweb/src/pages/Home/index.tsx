76c93ea063b4f7fa12af15d59e8215eedb066189
DSoaresD <danielsoares321@gmail.com>
2022-10-08 03:14:55 -0300

    Routes

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,35 +1,27 @@
+import { Link } from 'react-router-dom';
 import { ReactComponent as MainImage } from '../../assets/images/main-image.svg';
 import ButtonIcon from '../../components/ButtonIcon';
-import Navbar from '../../components/Navbar';
 import './styles.css';
 
 const Home = () => {
   return (
-    <>
-      <Navbar />
       <div className="home-container">
-        <div className="home-card">
-            <div className="home-content-container">
-                <div>
-                <h1> Conheça o melhor catalogo de produtos</h1>
-                <p>Ajudaremos voce a encontrar os melhores produtos do mercado</p>
-                </div>
-                <ButtonIcon/>
-
+        <div className="base-card home-card">
+          <div className="home-content-container">
+            <div>
+              <h1> Conheça o melhor catalogo de produtos</h1>
+              <p>Ajudaremos voce a encontrar os melhores produtos do mercado</p>
             </div>
-            <div className="home-image-container">
-                <MainImage />
-
-
-            </div>
-
-
+            <Link to="/products">
+              <ButtonIcon />
+            </Link>
+            
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
         </div>
-
-
-
       </div>
-    </>
   );
 };
 
