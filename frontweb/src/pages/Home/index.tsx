631b2d2dbbff6d6fc434ac5c667d21cbe028f11f
DSoaresD <danielsoares321@gmail.com>
2022-10-08 23:02:52 -0300

    Custom buttonIcon

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -5,23 +5,24 @@ import './styles.css';
 
 const Home = () => {
   return (
-      <div className="home-container">
-        <div className="base-card home-card">
-          <div className="home-content-container">
-            <div>
-              <h1> Conheça o melhor catalogo de produtos</h1>
-              <p>Ajudaremos voce a encontrar os melhores produtos do mercado</p>
-            </div>
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div>
+            <h1> Conheça o melhor catalogo de produtos</h1>
+            <p>Ajudaremos voce a encontrar os melhores produtos do mercado</p>
+          </div>
+          <div>
             <Link to="/products">
               <ButtonIcon />
             </Link>
-            
-          </div>
-          <div className="home-image-container">
-            <MainImage />
           </div>
         </div>
+        <div className="home-image-container">
+          <MainImage />
+        </div>
       </div>
+    </div>
   );
 };
 
