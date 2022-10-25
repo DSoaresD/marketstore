76c27c5cd778ca29894ad0515636bf6ac9855a14
DSoaresD <danielsoares321@gmail.com>
2022-10-24 23:31:37 -0300

    Check user authenticated

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 import { ReactComponent as MainImage } from '../../assets/images/main-image.svg';
 import ButtonIcon from '../../components/ButtonIcon';
+import { getTokenData, isAuthenticated } from '../../util/requests';
 import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
+
+      <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
+
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
