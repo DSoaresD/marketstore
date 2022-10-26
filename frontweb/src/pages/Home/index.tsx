42740d761255044759706a470b95d705635ff8c0
DSoaresD <danielsoares321@gmail.com>
2022-10-25 22:23:34 -0300

    Private Route

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -7,9 +7,6 @@ import './styles.css';
 const Home = () => {
   return (
     <div className="home-container">
-
-      <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
-
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
